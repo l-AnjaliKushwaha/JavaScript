29ae6007c69a6dcd35dc81c9e14194b6629ad6e2
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-01-31 18:33:48 +0530

    Loops

---
+++ 04_loops.js/03_Three.js
@@ -0,0 +1,46 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1,2,3,4,5,6]
+
+for (const num of arr) {
+//            console.log(num); 
+}
+
+const greetings = "Hello world!"
+
+for (const greet of greetings) {
+            if( greet == " "){
+                        // break;
+                        continue
+            }
+            // console.log(`Each char is ${greet}`); 
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United state of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+            console.log(key, ':-', value);
+}
+
+// object
+
+const myObject = {
+            'game1': 'NFS',
+            'game2': 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//             console.log(key, ':-', value);
+// }
+
